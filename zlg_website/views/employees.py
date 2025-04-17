a48acfc2ddbe58534a675305952460633268ac53
Alona Dmytrokhina <dmitrohina1708@gmail.com>
2025-04-17 12:50:13 +0300

    Added employee search

--- zlg_website/views/employees.py
+++ zlg_website/views/employees.py
@@ -9,10 +9,18 @@ from . import views
 def employees():
     if current_user.position != 'Менеджер':
         abort(403)
-    role_filter = request.args.get('position')  # 'Касир' або 'Менеджер'
-    if role_filter:
-        all_employees = Employee.query.filter_by(position=role_filter).order_by(Employee.id).all()
-    else:
-        all_employees = Employee.query.order_by(Employee.id).all()
 
-    return render_template("employees.html", user=current_user, employees=all_employees, current_filter=role_filter)
+    search_query = request.args.get('search', '')
+    position_filter = request.args.get('position')
+
+    query = Employee.query
+
+    if position_filter:
+        query = query.filter(Employee.position == position_filter)
+
+    if search_query:
+        query = query.filter(Employee.last_name.ilike(f"%{search_query}%"))
+
+    all_employees = query.order_by(Employee.id).all()
+
+    return render_template("employees.html", user=current_user, employees=all_employees, current_filter=position_filter)
